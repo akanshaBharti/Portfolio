47fef9933639e88a9c6e83c907945d738c56d5fe
Akansha <akanshabharti1213@gmail.com>
2024-05-07 16:49:27 +0530

    Css of contact page done issue resolved

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -11,12 +11,12 @@ const Contact = () => {
       style={{ backgroundImage: `url(${contactBg})` }}
     >
       <div className="absolute w-full h-screen bg-center">
-        <div className="relative z-20 flex flex-col items-center justify-center gap-6 pt-6">
-          <div className="absolute md:top-0 md:left-0 ml-4"> 
-          {/* hidden md:block  */}
-            <Sidebar />
+        <div className="relative z-20 flex flex-col items-center justify-center  pt-16">
+          <div className="absolute top-0 left-0 md:ml-4 hidden md:block">
+            {/* hidden md:block  */}
+            <Sidebar/>
           </div>
-          <h1 className="text-4xl md:text-5xl font-bold text-white text-center shadow-lg">
+          <h1 className="text-4xl md:text-5xl font-bold text-white text-center ">
             <span
               className="text-purple-600"
               style={{
@@ -28,57 +28,36 @@ const Contact = () => {
             </span>
           </h1>
 
-          {/* <div className="items-center justify-center">
-         <form className="">
-          <div>
-            <label className="text-2xl mr-2 ">Full Name: </label>
-            <input type="text"/>
-          </div>
-          <div>
-            <label className="text-2xl mr-2">Contact No:</label>
-            <input type="text"/>
-          </div>
-          <div>
-            <label className="text-2xl mr-2">Email:</label>
-            <input type="text"/>
-          </div>
-          <div>
-            <label className="text-2xl mr-2">Message:</label>
-            <input type="text"/>
-          </div>
-         </form>
-        </div> */}
-
-          <div className="flex items-center justify-center mt-20  ">
-            <form className="text-white ">
-              <div className="mb-4 flex flex-row">
-                <label className="text-2xl mr-2 ">Full Name:</label>
+          <div className="flex items-center justify-center md:mt-18 mt-14 ">
+            <form className="text-white md:p-8 ">
+              <div className="mb-4 md:flex md:flex-row md:gap-7 ">
+                <label className="text-xl md:text-2xl mr-2">Name:</label>
                 <input
                   type="text"
-                  className="bg-purple-400 bg-opacity-50  p-2 text-white w-full"
+                  className="bg-purple-400 bg-opacity-50 p-2 text-white md:w-96 w-full "
                 />
               </div>
-              <div className="mb-4 flex flex-row">
-                <label className="text-2xl mr-2">Contact No:</label>
+              <div className="mb-4 md:flex md:flex-row md:gap-3">
+                <label className="text-xl md:text-2xl mr-2">Contact:</label>
                 <input
                   type="text"
-                  className="bg-purple-400 bg-opacity-50  p-2 text-white w-full"
+                  className="bg-purple-400 bg-opacity-50  p-2 text-white md:w-full w-full"
                 />
               </div>
-              <div className="mb-4 flex flex-row">
-                <label className="text-2xl mr-2">Email:</label>
+              <div className="mb-4 md:flex md:flex-row md:gap-9">
+                <label className="text-xl md:text-2xl mr-2">Email:</label>
                 <input
                   type="text"
-                  className="bg-purple-400 bg-opacity-50  p-2 text-white w-full"
+                  className="bg-purple-400 bg-opacity-50  p-2 text-white md:w-full w-full"
                 />
               </div>
-              <div className="mb-4 flex flex-row">
-                <label className="text-2xl mr-2">Message:</label>
-                <textarea className="bg-purple-400 bg-opacity-50  p-2 text-white w-full"></textarea>
+              <div className="mb-4 md:flex md:flex-row ">
+                <label className="text-xl md:text-2xl mr-2">Message:</label>
+                <textarea className="bg-purple-400 bg-opacity-50  p-2 text-white md:w-full w-full"></textarea>
               </div>
               <button
                 type="submit"
-                className="bg-purple-700 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded"
+                className="bg-purple-700 hover:bg-purple-900 text-white font-bold py-2 px-6 rounded"
               >
                 Send
               </button>
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -10,9 +10,9 @@ const Sidebar = () =>{
     return( 
         <div className='pt-32'>
             <Link to="https://github.com/akanshaBharti" target='blank'><img src={githubIcon} alt='github' className='py-4 px-4'/></Link>
-            <Link to="https://www.linkedin.com/in/akansha-bharti/" target='blank'><img src={linkedinIcon} alt='github' className='py-4 px-4'/></Link>
-            <Link to="https://twitter.com/Akansha_1213" target='blank'><img src={twitterIcon} alt='github' className='py-4 px-4'/></Link>
-            <Link to="akanshabharti1213@gmail.com" target='blank'><img src={gmailIcon} alt='github' className='py-4 px-4'/></Link>
+            <Link to="https://www.linkedin.com/in/akansha-bharti/" target='blank'><img src={linkedinIcon} alt='linkedin' className='py-4 px-4'/></Link>
+            <Link to="https://twitter.com/Akansha_1213" target='blank'><img src={twitterIcon} alt='twitter' className='py-4 px-4'/></Link>
+            <Link to="akanshabharti1213@gmail.com" target='blank'><img src={gmailIcon} alt='gmail' className='py-4 px-4'/></Link>
 
         </div>
     ) 
