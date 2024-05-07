56df8d3660ded51149c47e380a6af959add15e58
Akansha <akanshabharti1213@gmail.com>
2024-05-07 23:27:35 +0530

    Contact form half done

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,33 +1,41 @@
-import React, { useEffect, useState } from "react";
+import React, { useState, useRef } from "react";
 import contactBg from "../../images/contact_bg.png";
 import { Button } from "@material-tailwind/react";
 import Navbar from "../Navbar/Navbar";
 import Sidebar from "../Sidebar/Sidebar";
+import emailjs from "@emailjs/browser";
 
 const Contact = () => {
-  const [name, setName] = useState();
-  const [contact, setContact] = useState();
-  const [email, setEmail] = useState();
-  const [message, setMessage] = useState();
+  const form = useRef();
+  const [name, setName] = useState("");
+  const [contact, setContact] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
 
-  const validateEmail = (email) => {
-    const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
-    return emailRegex.test(email);
-  };
+  // const validateEmail = (email) => {
+  //   const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
+  //   return emailRegex.test(email);
+  // };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    const authDetails = {
-      name : name,
-      contact : contact,
-      email: email,
-      message: message,
-    };
-    // login(authDetails);
-    // setIsEmailValid(validateEmail(email));
+    emailjs
+      .sendForm(
+        "service_mkmmv7h",
+        "template_wbly34v",
+        form.current,
+        "DI_HBtf80DLmjRnyD"
+      )
+      .then(
+        () => {
+          console.log("SUCCESS!");
+        },
+        (error) => {
+          console.log("FAILED", error.text);
+          // Add logic to show error message
+        }
+      );
   };
-
-  
 
   return (
     <div
@@ -36,10 +44,10 @@ const Contact = () => {
     >
       <div className="absolute w-full h-screen bg-center">
         <div className="relative z-20 flex flex-col items-center justify-center  pt-16">
-          <div className="absolute top-0 left-0 md:ml-4 hidden md:block">
-            {/* hidden md:block  */}
-            <Sidebar/>
+          <div className="absolute md:top-0 md:left-0 md:ml-4 md:block hidden">
+            <Sidebar />
           </div>
+
           <h1 className="text-4xl md:text-5xl font-bold text-white text-center ">
             <span
               className="text-purple-600"
@@ -53,11 +61,18 @@ const Contact = () => {
           </h1>
 
           <div className="flex items-center justify-center md:mt-18 mt-14 ">
-            <form className="text-white md:p-8 ">
+            <form
+              className="text-white md:p-8 "
+              ref={form}
+              onSubmit={handleSubmit}
+            >
               <div className="mb-4 md:flex md:flex-row md:gap-7 ">
                 <label className="text-xl md:text-2xl mr-2">Name:</label>
                 <input
                   type="text"
+                  name="name"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
                   className="bg-purple-400 bg-opacity-50 p-2 text-white md:w-96 w-full "
                 />
               </div>
@@ -65,6 +80,9 @@ const Contact = () => {
                 <label className="text-xl md:text-2xl mr-2">Contact:</label>
                 <input
                   type="text"
+                  name="contact"
+                  value={contact}
+                  onChange={(e) => setContact(e.target.value)}
                   className="bg-purple-400 bg-opacity-50  p-2 text-white md:w-full w-full"
                 />
               </div>
@@ -72,17 +90,27 @@ const Contact = () => {
                 <label className="text-xl md:text-2xl mr-2">Email:</label>
                 <input
                   type="text"
+                  name="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
                   className="bg-purple-400 bg-opacity-50  p-2 text-white md:w-full w-full"
                 />
               </div>
               <div className="mb-4 md:flex md:flex-row ">
                 <label className="text-xl md:text-2xl mr-2">Message:</label>
-                <textarea className="bg-purple-400 bg-opacity-50  p-2 text-white md:w-full w-full"></textarea>
+                <textarea
+                  name="message"
+                  value={message}
+                  onChange={(e) => setMessage(e.target.value)}
+                  className="bg-purple-400 bg-opacity-50  p-2 text-white md:w-full w-full"
+                ></textarea>
               </div>
               <button
-                onClick={handleSubmit}
+                // onClick={handleSubmit}
                 type="submit"
+                // value="send"
                 className="bg-purple-700 hover:bg-purple-900 text-white font-bold py-2 px-6 rounded"
+                
               >
                 Send
               </button>
